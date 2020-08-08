04c7902a5eb9cf61669626c28edb94646fed2659
Aravind Venugopal <aravindvenugopal02@gmail.com>
2020-08-08 12:13:43 +0530

    added notifications

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:oorja/complaints.dart';
-
-import 'notifications.dart';
-
+import 'package:oorja/notifications.dart';
+import 'complaints.dart';
+//import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() => runApp(Home());
-
 
 class Home extends StatefulWidget {
   @override
@@ -18,9 +18,11 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text("OORJA", style: TextStyle(fontSize: 25),),
+        title: Text(
+          "OORJA",
+          style: TextStyle(fontSize: 25),
+        ),
       ),
-
       drawer: Drawer(
         child: ListView(
           // Important: Remove any padding from the ListView.
@@ -143,19 +145,265 @@ class _HomeState extends State<Home> {
                 },
               ),
             ),
-
           ],
         ),
       ),
-
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.indigo,
-            height: 200,
-            width: 200,
-            child: Text("Oorja"),
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(25),
+                  bottomLeft: Radius.circular(25)),
+              child: Container(
+                color: Colors.black,
+                child: Card(
+                  margin: EdgeInsets.fromLTRB(10, 10, 10, 40),
+                  elevation: 5,
+                  child: SizedBox(
+                    height:
+                        MediaQuery.of(context).size.height > 800 ? 400 : 210,
+                    width: double.infinity,
+                    child: Carousel(
+                      images: [
+                        AssetImage('images/oorja.png'),
+                        AssetImage('images/oorja.png'),
+                        AssetImage('images/oorja.png'),
+                        AssetImage('images/oorja.png'),
+                      ],
+                      showIndicator: false,
+                      indicatorBgPadding: 3,
+                      borderRadius: true,
+                      radius: Radius.circular(4),
+                      dotSize: 5,
+                      dotSpacing: 20,
+                      dotColor: Colors.white70,
+                      dotIncreasedColor: Colors.white.withOpacity(.8),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                  child: Text(
+                    'Updates',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  )),
+            ),
+            GestureDetector(
+              onTap: () async {},
+              child: Card(
+                elevation: 5,
+                margin: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      leading: Image(
+                        image: NetworkImage(
+                            'https://m.economictimes.com/thumb/msid-67394105,width-1200,height-900,resizemode-4,imgsize-68202/complaint-getty.jpg'),
+                      ),
+                      title: Text(
+                        'Complaints',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      subtitle: Text(
+                        'None Pending',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () async {},
+              child: Card(
+                elevation: 5,
+                margin: EdgeInsets.fromLTRB(10, 5, 5, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      leading: Image(
+                        image: NetworkImage(
+                            'https://m.economictimes.com/thumb/msid-67394105,width-1200,height-900,resizemode-4,imgsize-68202/complaint-getty.jpg'),
+                      ),
+                      title: Text(
+                        'Complaints - 01',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      subtitle: Text('Issue Solved : Line Broken',
+                          style: TextStyle(fontSize: 14)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                  child: Text(
+                    "Get access to your billing, complaints or ongoing requets",
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.8),
+                      fontSize: 16,
+                    ),
+                  )),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        child: ClipRRect(
+                          child: GestureDetector(
+                            child: Container(
+                              width: 165,
+                              child: Text(
+                                "Complaints",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              color: Colors.black,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Complaints(),
+                                ),
+                              );
+                              // createInterstitialAd()
+                              //   ..load()
+                              //  ..show();
+                            },
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                        child: ClipRRect(
+                          child: GestureDetector(
+                            child: Container(
+                              width: 165,
+                              child: Text(
+                                "Billing",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              color: Colors.black,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Home(),
+                                ),
+                              );
+                              // createInterstitialAd()
+                              //  ..load()
+                              //  ..show();
+                            },
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        child: ClipRRect(
+                          child: GestureDetector(
+                            child: Container(
+                              width: 165,
+                              child: Text(
+                                "Notifications",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              color: Colors.black,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Notifications(),
+                                ),
+                              );
+                              // createInterstitialAd()
+                              // ..load()
+                              // ..show();
+                            },
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 5, 0, 10),
+                        child: ClipRRect(
+                          child: GestureDetector(
+                            child: Container(
+                              width: 165,
+                              child: Text(
+                                "History",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                              color: Colors.black,
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Home(),
+                                ),
+                              );
+                              //createInterstitialAd()
+                              //  ..load()
+                              // ..show();
+                            },
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+          ],
         ),
       ),
     );
--- lib/notifications.dart
+++ lib/notifications.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Notifications());
 
-
 class Notifications extends StatefulWidget {
   @override
   _NotificationsState createState() => _NotificationsState();
@@ -12,14 +11,127 @@ class _NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text("OORJA", style: TextStyle(fontSize: 20),),
-      ),
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text("Notifications - Coming Soon", style: TextStyle(fontSize: 30,color: Colors.white),),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            "Notifications",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () async {},
+                child: Card(
+                  elevation: 5,
+                  margin: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        leading: Image(
+                          image: NetworkImage(
+                              'https://www.pinclipart.com/picdir/big/153-1530123_automated-alerts-icon-clipart.png'),
+                        ),
+                        title: Text(
+                          'Power Outage - 08/08/2020',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        subtitle: Text(
+                          'There will be a power outage from 12:00pm to........',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {},
+                child: Card(
+                  elevation: 5,
+                  margin: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        leading: Image(
+                          image: NetworkImage(
+                              'https://www.pinclipart.com/picdir/big/153-1530123_automated-alerts-icon-clipart.png'),
+                        ),
+                        title: Text(
+                          'Maintainance - 06/08/2020',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        subtitle: Text(
+                          'There is a maintaianace scheduled from 2:00pm to........',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {},
+                child: Card(
+                  elevation: 5,
+                  margin: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        leading: Image(
+                          image: NetworkImage(
+                              'https://www.pinclipart.com/picdir/big/153-1530123_automated-alerts-icon-clipart.png'),
+                        ),
+                        title: Text(
+                          'Power Outage - 01/08/2020',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        subtitle: Text(
+                          'There will be a power outage from 9:00am to........',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {},
+                child: Card(
+                  elevation: 5,
+                  margin: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        leading: Image(
+                          image: NetworkImage(
+                              'https://www.pinclipart.com/picdir/big/153-1530123_automated-alerts-icon-clipart.png'),
+                        ),
+                        title: Text(
+                          'Line Broken - 27/07/2020',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        subtitle: Text(
+                          'Line broken at Nalancira due to heavy storm.',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
